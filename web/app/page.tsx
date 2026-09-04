256ca32d1be729bf7883fb819364dc0d0ec3d45a
MSchmalzDev <marcel.schmalz@outlook.com>
2026-09-04 12:36:14 +0200

    refactor: rename component files to PascalCase

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,5 @@
-import { Header } from "@/components/header";
-import { Hero } from "@/components/home/hero";
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/home/Hero";
 
 export default function Home() {
   return (
--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { FaGithub } from "react-icons/fa";
 import { Package } from "lucide-react";
 
