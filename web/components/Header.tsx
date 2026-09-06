882eabfb32b2591321ebdb530b41604874477873
MSchmalzDev <marcel.schmalz@outlook.com>
2026-09-06 20:59:52 +0200

    fix: button import for Header

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { FaGithub } from "react-icons/fa";
 import { Package } from "lucide-react";
 
